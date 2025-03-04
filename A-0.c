02f4bd5ebb219d652e208f8f9a2fb85f560c9ab5
MayurBedare <140893488+MayurBedare@users.noreply.github.com>
2025-03-04 18:36:55 +0530

    Pointers Assignments 3 and 4

---
+++ A-0.c
@@ -0,0 +1,20 @@
+
+
+/* 
+    Input -> 
+                             
+*/
+
+#include<stdio.h>
+
+int main()
+{
+    //code
+
+    return 0;
+}
+
+/*
+    Output -> 
+                                
+*/
