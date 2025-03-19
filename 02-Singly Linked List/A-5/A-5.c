d17fd9fc638f30f50d405c4cb9a1cf6ae75652ff
MayurBedare <140893488+MayurBedare@users.noreply.github.com>
2025-03-19 12:02:29 +0530

    Singly LInked List Assignment No. 5

---
+++ 02-Singly Linked List/A-5/A-5.c
@@ -0,0 +1,139 @@
+/*
+    A-5 ->  write a c program to Create a singly linked list and search a particular node. 
+          
+            I/P  ---->   |_1_|_AA_|_100_| --> |_2_|_BB_|_200_| --> |_3_|_CC_|_300_| --> |_4_|_DD_|_400_| --> |_5_|_EE_|_500_| --> |_6_|_FF_|_NULL_|  
+            Enter : Key = 5
+            O/P ---->  
+            |_5_|_EE_|_500_|
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct SLLNode
+{
+    int number;
+    char name[20];
+    struct SLLNode *next;
+} SLLNode;
+
+SLLNode* createSLL();
+void displaySLL(SLLNode*);
+void freeSLL(SLLNode*);
+void search(SLLNode*);
+
+int main() {
+    
+    SLLNode *head = NULL;
+
+    head = createSLL();
+
+    printf("\n\tLinked List :");
+    displaySLL(head);
+
+    search(head);
+
+    freeSLL(head);
+
+    return 0;
+    
+}
+
+SLLNode* createSLL() {
+
+    SLLNode *head, *nw, *last;
+    head = NULL, nw = NULL, last = NULL;
+
+    int cnt;
+
+    do
+    {
+        nw = malloc(sizeof(SLLNode));
+
+        printf("\n\tEnter Number and Name : ");
+        scanf("%d %s", &(nw -> number), nw -> name);
+
+        nw -> next = NULL;
+
+        if (head == NULL)
+            head = nw;
+        else 
+            last -> next = nw;
+        
+        last = nw;
+
+        printf("Do you want to enter more records ( yes(1) / no(0) ) : ");
+        scanf("%d", &cnt);
+    } while (cnt != 0);
+
+    return head;
+
+}
+
+void search(SLLNode *head) {
+
+    SLLNode *s = NULL;
+    s = head;
+    
+    int key;
+    printf("\n\tEnter : Key = ");
+    scanf("%d", &key);
+
+    for (int i=1 ; i < key && s != NULL ; s = s -> next, i++);
+
+    if (s != NULL) {
+        printf("\n|_Head_| --> |_%d_|_%s_|_%p_| \n\n", s -> number, s -> name, s -> next);
+    } else {
+        printf("\n\tInvalid key entered!\n\n");
+        free(s);
+        return;
+    }
+
+}
+
+void displaySLL(SLLNode *d) {
+
+    printf("\n\n|_Head_| ");
+    for ( ; d != NULL ; d = d -> next )
+        printf("--> |_%d_|_%s_|_%p_| ", d -> number, d -> name, d -> next);
+
+    printf("\n\n");
+
+}
+
+void freeSLL(SLLNode *f) {
+
+    for( ; f != NULL ; f = f -> next)
+        free(f);
+
+    f = NULL;
+}
+
+/*
+    Output ->  
+                Enter Number and Name : 1 AA
+            Do you want to enter more records ( yes(1) / no(0) ) : 1
+
+                    Enter Number and Name : 2 BB
+            Do you want to enter more records ( yes(1) / no(0) ) : 1
+
+            Do you want to enter more records ( yes(1) / no(0) ) : 1
+
+                    Enter Number and Name : 4 DD
+            Do you want to enter more records ( yes(1) / no(0) ) : 1
+
+                    Enter Number and Name : 5 EE
+            Do you want to enter more records ( yes(1) / no(0) ) : 1
+
+                    Enter Number and Name : 6 FF
+            Do you want to enter more records ( yes(1) / no(0) ) : 0
+
+                    Linked List :
+
+            |_Head_| --> |_1_|_AA_|_00C10D40_| --> |_2_|_BB_|_00C10D68_| --> |_3_|_CC_|_00C10D90_| --> |_4_|_DD_|_00C10DB8_| --> |_5_|_EE_|_00C10DE0_| --> |_6_|_FF_|_00000000_|
+
+
+                    Enter : Key = 5
+
+            |_Head_| --> |_5_|_EE_|_00C10DE0_|                              
+*/
