7e773ed849255b3805e7bbe67c71a703c4ae7b91
MayurBedare <140893488+MayurBedare@users.noreply.github.com>
2025-04-04 16:07:53 +0530

    Doubly Linked List Assignment No. 1

---
+++ 03-Doubly Linked List/01-Assignment/A-1.c
@@ -0,0 +1,105 @@
+/*
+    A-1 ->  write a c program to Create Doubly linked list and Display it.
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct DLLNode
+{
+    int number;
+    char name[20];
+    struct DLLNode *next;
+    struct DLLNode *prev;
+} DLLNode;
+
+DLLNode* createDLL();
+void displayDLL(DLLNode*);
+void freeDLL(DLLNode*);
+
+int main() {
+    
+    DLLNode *head = NULL;
+
+    head = createDLL();
+
+    printf("\n\tLinked List : ");
+    displayDLL(head);
+    
+    freeDLL(head);
+
+    return 0;
+    
+}
+
+DLLNode* createDLL() {
+
+    DLLNode *head, *nw, *last;
+    head = NULL, nw = NULL, last = NULL;
+
+    int cnt;
+
+    do
+    {
+        nw = malloc(sizeof(DLLNode));
+
+        printf("\n\tEnter Number and Name : ");
+        scanf("%d %s", &(nw -> number), nw -> name);
+
+        nw -> next = NULL;
+
+        if (head == NULL) {
+            head = nw;
+            nw -> prev = NULL;
+        }
+        else {
+            last -> next = nw;
+            nw -> prev = last;
+        }
+        
+        last = nw;
+
+        printf("Do you want to enter more records ( yes(1) / no(0) ) : ");
+        scanf("%d", &cnt);
+    } while (cnt != 0);
+
+    return head;
+
+}
+
+void displayDLL(DLLNode *d) {
+
+    printf("\n\n|_Head_| ");
+    for ( ; d != NULL ; d = d -> next )
+        printf("<--> |_%p_|_%d_|_%s_|_%p_| ", d -> prev, d -> number, d -> name, d -> next);
+
+    printf("\n\n");
+
+}
+
+void freeDLL(DLLNode *f) {
+
+    for( ; f != NULL ; f = f -> next)
+        free(f);
+
+    f = NULL;
+}
+
+/*
+    Output ->    
+                    Enter Number and Name : 1 AA
+            Do you want to enter more records ( yes(1) / no(0) ) : 1
+
+                    Enter Number and Name : 2 BB
+            Do you want to enter more records ( yes(1) / no(0) ) : 1
+
+                    Enter Number and Name : 3 CC
+            Do you want to enter more records ( yes(1) / no(0) ) : 1
+
+                    Enter Number and Name : 4 DD
+            Do you want to enter more records ( yes(1) / no(0) ) : 0
+
+                    Linked List : 
+
+            |_Head_| <--> |_00000000_|_1_|_AA_|_00D20D40_| <--> |_00D22F98_|_2_|_BB_|_00D20D68_| <--> |_00D20D40_|_3_|_CC_|_00D20D90_| <--> |_00D20D68_|_4_|_DD_|_00000000_|                    
+*/
