122ae168fa68d089f3fc5140c7cb4f7703f56ed8
MayurBedare <140893488+MayurBedare@users.noreply.github.com>
2025-04-09 18:05:01 +0530

    Singly Circular Linked List Assignment No. 1

---
+++ 04-Singly Circular Linked List/01-Assignment/A-1.c
@@ -0,0 +1,116 @@
+/*
+    A-1 ->  write a c program to Create and display circular singly linked list.  
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct SCLLNode
+{
+    int number;
+    char name[20];
+    struct SCLLNode *next;
+} SCLLNode;
+
+SCLLNode* createSCLL();
+void displaySCLL(SCLLNode*);
+void freeSCLL(SCLLNode*);
+
+
+int main() {
+   
+    SCLLNode *last = NULL;
+
+    last = createSCLL();
+
+    printf("\n\t Singly Circular Linked List : ");
+    displaySCLL(last);
+    
+    freeSCLL(last);
+
+    return 0;
+    
+}
+
+SCLLNode* createSCLL() {
+
+    SCLLNode *head = NULL,*nw = NULL, *last = NULL;
+    
+    int cnt;
+
+    do
+    {
+        nw = malloc(sizeof(SCLLNode));
+
+        printf("\n\tEnter Number and Name : ");
+        scanf("%d %s", &(nw -> number), nw -> name);
+
+        nw -> next = nw;
+
+        if (head == NULL) {
+            head = nw;
+        }
+        else {
+            last -> next = nw;
+            nw -> next = head;
+        }
+        
+        last = nw;
+
+        printf("Do you want to enter more records ( yes(1) / no(0) ) : ");
+        scanf("%d", &cnt);
+    } while (cnt != 0);
+
+    return last;
+
+}
+
+void displaySCLL(SCLLNode *last) {
+    
+    SCLLNode *d = NULL;
+    d = last -> next;
+    printf("\n\n|_Head_| ");
+
+    for ( ; d != last ; d = d -> next )
+        printf("--> |_%d_|_%s_|_%p_| ", d -> number, d -> name, d -> next);
+
+    printf("--> |_%d_|_%s_|_%p_| <-- |_last_|", d -> number, d -> name, d -> next);
+
+    printf("\n\n");
+
+}
+
+void freeSCLL(SCLLNode *last) {
+
+    if (last == NULL)
+        return;
+
+    SCLLNode *f = NULL,*t = NULL;
+    f = last -> next;
+
+    while (f != last) {
+        t = f;   
+        f = f -> next;
+        free(t);
+        t = NULL;
+    }
+    
+    free(f);
+    f = NULL;
+}
+
+/*
+    Output ->
+                    Enter Number and Name : 1 AA
+            Do you want to enter more records ( yes(1) / no(0) ) : 1
+
+                    Enter Number and Name : 2 BB
+            Do you want to enter more records ( yes(1) / no(0) ) : 1
+
+                    Enter Number and Name : 3 CC
+            Do you want to enter more records ( yes(1) / no(0) ) : 0
+
+                    Singly Circular Linked List : 
+
+            |_Head_| --> |_1_|_AA_|_00B91388_| --> |_2_|_BB_|_00B913B0_| --> |_3_|_CC_|_00B92FC0_| <-- |_last_|
+*/
