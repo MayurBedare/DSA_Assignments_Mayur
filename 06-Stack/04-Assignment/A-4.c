bd635de522e6952ba4e8035a7399d82ae96605db
MayurBedare <140893488+MayurBedare@users.noreply.github.com>
2025-04-21 22:59:13 +0530

    Stack Assignment No. 4

--- 06-Stack/04-Assignment/A-4.c
+++ 06-Stack/04-Assignment/A-4.c
@@ -1,9 +1,9 @@
 /*
-    A-4 -> Write the code to covert the below expression from Infix to Prefix and Postfix 
+    A-4 -> Write the code to covert the below expression from Infix to Postfix 
 */
 
 /* 
-    Input ->  F*(A+B)/E-D*(G+H/K)                      
+    Input ->  F*(A+B)/E-D*(G+H/K)                     
 */
 
 #include<stdio.h>
@@ -11,19 +11,16 @@
 
 int precedence(char,char);
 int priority(char);
-char* operandPrefixStack(char[][30],int*,char);
 char* operandPostfixStack(char[][30],int*,char);
 
 int main() {
     int stk = -1;
-    int prefix = -1;
     int postfix = -1;
     char arr[30];
     char stkOperator[10];
-    char stkPrefixOperand[10][30];
     char stkPostfixOperand[10][30];
 
-    printf("\n\t ***** Infix -> Prefix & Postfix *****");
+    printf("\n\t ***** Infix -> Postfix *****");
     printf("\n\t Enter the Infix Equation : ");
     scanf("%s",arr);
 
@@ -37,7 +34,6 @@ int main() {
             }
             if (arr[i] == ')') {
                 while (stkOperator[stk] != '(') {
-                    strcpy(stkPrefixOperand[prefix],operandPrefixStack(stkPrefixOperand,&prefix,stkOperator[stk]));
                     strcpy(stkPostfixOperand[postfix],operandPostfixStack(stkPostfixOperand,&postfix,stkOperator[stk]));
                     stk--;
                 }
@@ -46,7 +42,6 @@ int main() {
             if (arr[i] == '+' || arr[i] == '-' || arr[i] == '/' || arr[i] == '*' || arr[i] == '^') {
                 if (stkOperator[stk] == '+' || stkOperator[stk] == '-' || stkOperator[stk] == '*' || stkOperator[stk] == '/' || stkOperator[stk] == '^') {
                     if (precedence(arr[i],stkOperator[stk])) {
-                        strcpy(stkPrefixOperand[prefix],operandPrefixStack(stkPrefixOperand,&prefix,stkOperator[stk]));
                         strcpy(stkPostfixOperand[postfix],operandPostfixStack(stkPostfixOperand,&postfix,stkOperator[stk]));
                         stkOperator[stk] = arr[i];
                     } else {
@@ -59,20 +54,17 @@ int main() {
                 }
             }
         } else {
-            strcpy(stkPrefixOperand[prefix],operandPrefixStack(stkPrefixOperand,&prefix,arr[i]));
             strcpy(stkPostfixOperand[postfix],operandPostfixStack(stkPostfixOperand,&postfix,arr[i]));
         }
     }
     while (stk != -1) {
         if (stkOperator[stk] != '(' && stk >= 0) {
-            strcpy(stkPrefixOperand[prefix],operandPrefixStack(stkPrefixOperand,&prefix,stkOperator[stk]));
             strcpy(stkPostfixOperand[postfix],operandPostfixStack(stkPostfixOperand,&postfix,stkOperator[stk]));
             stk--;
         }
     }
     
-    printf("\n\t The Prefix Expression of the given Infix Expression \t: %s",stkPrefixOperand[prefix]);
-    printf("\n\n\t The Postfix Expression of the given Infix Expression \t: %s\n\n",stkPostfixOperand[postfix]);
+    printf("\n\t The Postfix Expression of the given Infix Expression : %s\n\n",stkPostfixOperand[postfix]);
 
     return 0;
 }
@@ -89,33 +81,15 @@ int precedence(char i,char j) {
 int priority(char ch) {
     int p;
     if (ch == '+' || ch == '-') p = 1;
-    if (ch == '*' || ch == '/') p = 2;
+    if (ch == '*' || ch == '/' || ch == '%') p = 2;
     if (ch == '^')  p = 3;  
     return p;
-}
-
-char* operandPrefixStack(char stkPrefixOperand[][30],int *prefix,char i) {
-    char temp[2] = {i,'\0'};
-    char string[30];
-    if (i == '+' || i == '-' || i == '*' || i == '/' || i == '^') {
-        strcpy(string,temp);
-        (*prefix)--;
-        strcat(string,stkPrefixOperand[(*prefix)]);
-        (*prefix)++;
-        strcat(string,stkPrefixOperand[(*prefix)]);
-        (*prefix)--;
-        strcpy(stkPrefixOperand[(*prefix)],string);
-    } else {
-        (*prefix)++;
-        strcpy(stkPrefixOperand[(*prefix)],temp);
-    }
-    return stkPrefixOperand[(*prefix)];
 }
 
 char* operandPostfixStack(char stkPostfixOperand[][30],int *postfix,char i) {
     char temp[2] = {i,'\0'};
     char string[30];
-    if (i == '+' || i == '-' || i == '*' || i == '/') {
+    if (i == '+' || i == '-' || i == '*' || i == '/' || i == '^' || i == '%') {
         (*postfix)--;
         strcpy(string,stkPostfixOperand[(*postfix)]);
         (*postfix)++;
@@ -131,10 +105,9 @@ char* operandPostfixStack(char stkPostfixOperand[][30],int *postfix,char i) {
 }
 
 /*
-    Output ->   ***** Infix -> Prefix & Postfix *****
+    Output ->    
+                ***** Infix -> Postfix *****
                 Enter the Infix Equation : F*(A+B)/E-D*(G+H/K)
 
-                The Prefix Expression of the given Infix Expression    : *F-/+ABE*D+G/HK
-
-                The Postfix Expression of the given Infix Expression   : FAB+E/DGHK/+*-*                            
+                The Postfix Expression of the given Infix Expression : FAB+*E/DGHK/+*-                    
 */
