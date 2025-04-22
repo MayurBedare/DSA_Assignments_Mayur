3b7af651e1c0c61f450a3e159e85881fd3c0cc0d
MayurBedare <140893488+MayurBedare@users.noreply.github.com>
2025-04-22 10:25:26 +0530

    Pointers Assignment No. 4

---
+++ 01-C Revision/01-Pointers/04-Assignment/A-4.c
@@ -0,0 +1,52 @@
+/*
+
+  write a  Program to count vowels and consonants in a string using pointer.
+
+*/
+
+/* 
+    Input -> User given Eg. Mayur Bedare CSE
+                             
+*/
+
+#include<stdio.h>
+
+int main()
+{
+    char string[50];
+    char *p = string;
+
+    printf("Enter the string to count vowels and consonants :");
+    fgets(p, 50, stdin);
+
+    int count_v = 0;
+    int count_c = 0;
+
+    for (int i=0; *(p+i) != '\0' ; i++)
+    {
+        if (*(p+i) == ' ' || *(p+i) == '\n')
+            continue;
+        
+        if(*(p+i) == 'a' || *(p+i) == 'e' || *(p+i) == 'i' || *(p+i) == 'o' || *(p+i) == 'u' 
+            || *(p+i) == 'A' || *(p+i) == 'E' || *(p+i) == 'I' || *(p+i) == 'O' || *(p+i) == 'U')
+        {
+            count_v++;
+        }
+        else
+        {
+            count_c++;
+        }
+    }
+
+    printf("The total number of vowels in the given string : %d \n",count_v);
+    printf("The total number of consonants in the given string : %d ",count_c);
+
+	return 0;
+}
+
+/*
+    Output -> 
+                Enter the string to count vowels and consonants :Mayur Bedare CSE
+                The total number of vowels in the given string : 6 
+                The total number of consonants in the given string : 8                                 
+*/
