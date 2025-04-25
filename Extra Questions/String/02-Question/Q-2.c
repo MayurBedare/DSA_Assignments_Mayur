a29bbec1c198dd6abd42acb9a47b963ebed10629
MayurBedare <140893488+MayurBedare@users.noreply.github.com>
2025-04-25 15:18:57 +0530

    String Question No. 2

---
+++ Extra Questions/String/02-Question/Q-2.c
@@ -0,0 +1,40 @@
+/*
+    Q-2 ->  Accept two strings and concatinate the contents into 3rd string
+*/
+
+/* 
+    Input ->    string1[] = "Hello"
+                string2[] = "World!"        
+*/
+
+#include<stdio.h>
+
+int main() {
+    char string1[50];
+    char string2[50];
+
+    printf("\n\t Enter the first string  : ");
+    scanf("%s",string1);
+    printf("\n\t Enter the second string : ");
+    scanf("%s",string2);
+
+    int i;
+    for (i = 0 ; string1[i] != '\0' ; i++);
+    for (int j = 0 ; string2[j] != '\0' ; j++,i++) {
+        string1[i] = string2[j];
+    }
+    string1[i] = '\0';
+
+    printf("\n\t The final concatenated string : %s \n\n",string1);
+
+    return 0;   
+}
+
+/*
+    Output ->    
+                Enter the first string  : Hello
+
+                Enter the second string : World!
+
+                The final concatenated string : HelloWorld!                            
+*/
