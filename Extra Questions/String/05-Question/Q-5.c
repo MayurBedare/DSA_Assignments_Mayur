37edf1f2425461d4920710a7656ea675ff8da7a3
MayurBedare <140893488+MayurBedare@users.noreply.github.com>
2025-04-25 20:03:25 +0530

    String Question No. 5

--- Extra Questions/String/05-Question/Q-5.c
+++ Extra Questions/String/05-Question/Q-5.c
@@ -16,7 +16,7 @@ int main() {
     size = i;
     for (int j = 0 ; j != size/2 ; j++) {
         if (string[j] == string[--i]) {
-            flag = 0;
+            flag = 1;
         } else {
             flag = 0;
             break;
