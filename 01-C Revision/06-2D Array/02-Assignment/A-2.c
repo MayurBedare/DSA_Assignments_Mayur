d975ccb3efd87419f4e06c56c87288839dd194eb
MayurBedare <140893488+MayurBedare@users.noreply.github.com>
2025-05-09 00:26:40 +0530

    2D Array Assignment No. 2

--- 01-C Revision/06-2D Array/02-Assignment/A-2.c
+++ 01-C Revision/06-2D Array/02-Assignment/A-2.c
@@ -66,7 +66,7 @@ int main() {
         for (int j = 0 ; j < M ; j++) {
             if (j == 0) printf("\n");
             printf("\t Enter arr_1[%d][%d] : ",i,j);
-            scanf("%d",&arr1[i][j]); // scanf("%d",(arr + i + j)); also same
+            scanf("%d",&arr1[i][j]); 
         }
     }
 
