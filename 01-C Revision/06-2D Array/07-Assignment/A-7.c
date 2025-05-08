2d48d25e4527092bb1af59c016afd016460a15c3
MayurBedare <140893488+MayurBedare@users.noreply.github.com>
2025-05-09 00:28:47 +0530

    2D Array Assignment No. 7

---
+++ 01-C Revision/06-2D Array/07-Assignment/A-7.c
@@ -0,0 +1,32 @@
+/*
+    A-7 ->  Accept  4*4 matrix and display the following patern
+
+            1 1 1 *
+            1 1 * 0
+            1 * 0 0
+            * 0 0 0  
+*/
+
+#include<stdio.h>
+
+#define N 4
+
+int main() {
+    int arr[N][N];
+
+    for (int i = 0 ; i < N ; i++) {
+        for (int j = 0 ; j < N ; j++) {
+            if (j == 0) printf("\n\t");
+
+            if (j < N - 1 - i)  printf("1 ");
+
+            if (j == N - 1 - i) printf("* ");
+            
+            if (j > N - 1 - i)    printf("0 ");
+        }
+    }
+    printf("\n\n");
+
+    return 0;   
+}
+
