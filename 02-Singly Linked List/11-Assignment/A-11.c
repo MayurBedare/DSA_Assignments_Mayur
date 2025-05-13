8ce3bca862f715e8af0f4d559af67e0f9fc6cf8b
MayurBedare <140893488+MayurBedare@users.noreply.github.com>
2025-05-13 11:42:26 +0530

    Singly Linked List Assignment No. 11

--- 02-Singly Linked List/11-Assignment/A-11.c
+++ 02-Singly Linked List/11-Assignment/A-11.c
@@ -2,11 +2,6 @@
     A-11 -> write a c program to Create a Singly linked list and Delete the node in the given position
 */
 
-/* 
-    Input -> |_Head_| --> |_1_|_AA_|_00B71388_| --> |_2_|_BB_|_00B713B0_| --> |_3_|_CC_|_00000000_|  
-    position = 2                 
-*/
-
 #include<stdio.h>
 #include<stdlib.h>
 
@@ -22,26 +17,52 @@ void displaySLL(SLLNode*);
 void freeSLL(SLLNode*);
 SLLNode* positionNodeDeletion(SLLNode*);
 
-int main() {
-    
+int main() { 
     SLLNode *head = NULL;
+
+    printf("\n\t ***** Delete a node at the given position : Singly Linked List ***** \n");
+
+    printf("\n\t -------------------------------------------------------------");
+    printf("\n\t Create a linked list to delete a node at the given position : \n");
+    printf("\t ------------------------------------------------------------- \n");
 
     head = createSLL();
 
-    printf("\n\tLinked List : ");
+    printf("\n\t ------------------------------------------------------------- \n");
+    printf("\n\t Linked List : ");
     displaySLL(head);
-
-    head = positionNodeDeletion(head);
-
-    if( head != NULL) {
-    printf("\n\tLinked List after Deletion of node at Given position : ");
-    displaySLL(head);
-    }
-
-    freeSLL(head);
-
-    return 0;
     
+    while (1) {
+        int ch; 
+        printf("\n\t --------------------------------------- ");
+        printf("\n\t 1. Delete a node at the given position ");
+        printf("\n\t 2. Exit ");
+        printf("\n\t --------------------------------------- ");
+        printf("\n\t Enter your choice : ");
+        scanf("%d",&ch);
+        printf("\t --------------------------------------- \n");
+        
+        switch (ch) {
+            case 1:
+                if (head == NULL) {
+                    printf("\n\t Linked list is empty! \n\n");
+                    exit(1);
+                }
+                head = positionNodeDeletion(head);
+                break;
+            
+            case 2:
+                freeSLL(head);
+                head = NULL;
+                printf("\n\t Program Exited Successfully. \n\n");
+                exit(0);
+
+            default: 
+                printf("\n\t Invalid Choice Entered! \n");
+                break;
+        }
+    }
+    return 0;
 }
 
 SLLNode* createSLL() {
@@ -53,9 +74,14 @@ SLLNode* createSLL() {
 
     do
     {
-        nw = malloc(sizeof(SLLNode));
+        nw = (SLLNode*)malloc(sizeof(SLLNode));
+        if (nw == NULL)
+        {
+            printf("\n\t Memory Allocation Failed! \n\n");
+            exit(1);
+        }
 
-        printf("\n\tEnter Number and Name : ");
+        printf("\n\t Enter Number and Name : ");
         scanf("%d %s", &(nw -> number), nw -> name);
 
         nw -> next = NULL;
@@ -67,7 +93,7 @@ SLLNode* createSLL() {
         
         last = nw;
 
-        printf("Do you want to enter more records ( yes(1) / no(0) ) : ");
+        printf("\n\t Do you want to enter more records ( yes(1) / no(0) ) : ");
         scanf("%d", &cnt);
     } while (cnt != 0);
 
@@ -80,12 +106,13 @@ SLLNode* positionNodeDeletion(SLLNode * head) {
     SLLNode *del = NULL,*temp = NULL;
     del = head;
 
-    int pos;
-    printf("\n\tEnter the position of the node to be deleted : ");
+    int pos,flag = 0;
+    printf("\n\t Enter the position of the node to be deleted : ");
     scanf("%d", &pos);
 
     if (pos == 1)
     {
+        flag = 1;
         head = del -> next;
         free(del);
         del = NULL;
@@ -93,14 +120,19 @@ SLLNode* positionNodeDeletion(SLLNode * head) {
         for (int i = 1 ; i < pos-1 && del -> next != NULL ; del = del -> next,i++);
 
         if (del -> next != NULL) {
+            flag = 1;
             temp = del -> next;
             del -> next = del -> next -> next;
             free(temp);
             temp = NULL;
         } else {
-            printf("\n\tInvalid Position Entered!\n\n");
-            exit(0);
+            printf("\n\t Invalid Position Entered! \n");
         }
+    }
+
+    if (flag == 1) {
+        printf("\n\t Linked list after Deletion of node at the given position : ");
+        displaySLL(head);
     }
 
     return head;
@@ -109,65 +141,21 @@ SLLNode* positionNodeDeletion(SLLNode * head) {
 
 void displaySLL(SLLNode *d) {
 
-    printf("\n\n|_Head_| ");
+    printf("\n\n\t |_Head_| ");
     for ( ; d != NULL ; d = d -> next )
         printf("--> |_%d_|_%s_|_%p_| ", d -> number, d -> name, d -> next);
 
-    printf("\n\n");
+    printf("\n");
 
 }
 
 void freeSLL(SLLNode *f) {
+    SLLNode *t = NULL;
 
-    for( ; f != NULL ; f = f -> next)
-        free(f);
-
-    f = NULL;
+    while (f != NULL) {
+        t = f;
+        f = f -> next;
+        free(t);
+    }
 }
 
-/*
-    Output ->      
-                    Enter Number and Name : 1 AA
-            Do you want to enter more records ( yes(1) / no(0) ) : 1
-
-                    Enter Number and Name : 2 BB
-            Do you want to enter more records ( yes(1) / no(0) ) : 1
-
-                    Enter Number and Name : 3 CC
-            Do you want to enter more records ( yes(1) / no(0) ) : 0
-
-                    Linked List :
-
-            |_Head_| --> |_1_|_AA_|_00B71388_| --> |_2_|_BB_|_00B713B0_| --> |_3_|_CC_|_00000000_|
-
-
-                    Enter the position of the node to be deleted : 2
-
-                    Linked List after Deletion of node at Given position :
-
-            |_Head_| --> |_1_|_AA_|_00B713B0_| --> |_3_|_CC_|_00000000_|    
-*/
-
-/*
-    Position = 1 (Head Position) :
-
-        Linked List : 
-
-    |_Head_| --> |_2_|_BB_|_00B713B0_| --> |_3_|_CC_|_00000000_|  
-*/
-
-/*
-    Position = 2 (Middle Position) :
-
-        Linked List : 
-
-    |_Head_| --> |_1_|_AA_|_00B71388_| --> |_3_|_CC_|_00000000_| 
-*/
-
-/*
-    Position = 3 (End Position) :
-
-        Linked List : 
-
-    |_Head_| --> |_1_|_AA_|_00B71388_| --> |_2_|_BB_|_00000000_|  
-*/
