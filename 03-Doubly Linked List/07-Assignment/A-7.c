0943ffd14de22ff1fda36dcef8a8c0617da992c4
MayurBedare <140893488+MayurBedare@users.noreply.github.com>
2025-05-13 11:48:12 +0530

    Doubly Linked List Assignment No. 7

--- 03-Doubly Linked List/07-Assignment/A-7.c
+++ 03-Doubly Linked List/07-Assignment/A-7.c
@@ -52,6 +52,11 @@ DLLNode* createDLL() {
     do
     {
         nw = (DLLNode*)malloc(sizeof(DLLNode));
+        if (nw == NULL)
+        {
+            printf("\n\t Memory Allocation Failed! \n\n");
+            exit(1);
+        }
 
         printf("\n\tEnter Number and Name : ");
         scanf("%d %s", &(nw -> number), nw -> name);
@@ -121,11 +126,13 @@ void displayDLL(DLLNode *d) {
 }
 
 void freeDLL(DLLNode *f) {
+    DLLNode *t = NULL;
 
-    for( ; f != NULL ; f = f -> next)
-        free(f);
-
-    f = NULL;
+    while (f != NULL) {
+        t = f;
+        f = f -> next;
+        free(t);
+    }
 }
 
 /*
