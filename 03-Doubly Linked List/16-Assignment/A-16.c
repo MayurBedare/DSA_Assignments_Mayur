21a0099a75240611fd31c5680b2e9328f71e3936
MayurBedare <140893488+MayurBedare@users.noreply.github.com>
2025-05-14 12:29:00 +0530

    Doubly Linked List Assignment No. 16

--- 03-Doubly Linked List/16-Assignment/A-16.c
+++ 03-Doubly Linked List/16-Assignment/A-16.c
@@ -39,6 +39,7 @@ int main() {
     swapDLL(head);
     
     freeDLL(head);
+    head = NULL;
 
     return 0;
     
@@ -53,7 +54,12 @@ DLLNode* createDLL() {
 
     do
     {
-        nw = malloc(sizeof(DLLNode));
+        nw = (DLLNode*)malloc(sizeof(DLLNode));
+        if (nw == NULL)
+        {
+            printf("\n\t Memory Allocation Failed! \n\n");
+            exit(1);
+        }
 
         printf("\n\tEnter Number and Name : ");
         scanf("%d %s", &(nw -> number), nw -> name);
@@ -121,11 +127,13 @@ void displayDLL(DLLNode *d) {
 }
 
 void freeDLL(DLLNode *f) {
+    DLLNode *t = NULL;
 
-    for( ; f != NULL ; f = f -> next)
-        free(f);
-
-    f = NULL;
+    while (f != NULL) {
+        t = f;
+        f = f -> next;
+        free(t);
+    }
 }
 
 /*
