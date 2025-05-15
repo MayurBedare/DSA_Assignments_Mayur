cf3b15d70b319b5eeac711fb97b272d0980f5121
MayurBedare <140893488+MayurBedare@users.noreply.github.com>
2025-05-15 10:53:17 +0530

    Singly Circular Linked List Assignment No. 7

--- 04-Singly Circular Linked List/07-Assignment/A-7.c
+++ 04-Singly Circular Linked List/07-Assignment/A-7.c
@@ -42,6 +42,7 @@ int main() {
     displaySCLL(last);
 
     freeSCLL(last);
+    last = NULL;
 
     return 0;
     
@@ -56,6 +57,10 @@ SCLLNode* createSCLL() {
     do
     {
         nw = (SCLLNode*)malloc(sizeof(SCLLNode));
+        if (nw == NULL) {
+            printf("\n\t Memory Allocation Failed! \n\n");
+            exit(1);
+        }
 
         printf("\n\tEnter Number and Name : ");
         scanf("%d %s", &(nw -> number), nw -> name);
