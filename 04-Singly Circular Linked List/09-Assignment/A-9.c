e5a4fb0a8b4922ab7e34f6c85cdd086db8220cc2
MayurBedare <140893488+MayurBedare@users.noreply.github.com>
2025-05-15 10:54:25 +0530

    Singly Circular Linked List Assignment No. 9

--- 04-Singly Circular Linked List/09-Assignment/A-9.c
+++ 04-Singly Circular Linked List/09-Assignment/A-9.c
@@ -26,22 +26,48 @@ int main() {
    
     SCLLNode *last = NULL;
 
+    printf("\n\t ***** Delete a node at the given position : Singly Circular Linked List ***** \n");
+
+    printf("\n\t -------------------------------------------------------------");
+    printf("\n\t Create a linked list to insert a node at the given position : \n");
+    printf("\t ------------------------------------------------------------- \n");
+
     last = createSCLL();
 
-    printf("\n\t Singly Circular Linked List : ");
-    displaySCLL(last);
-
-    last = deletePos(last);
-
-    if (last == NULL) {
-        printf("\n\t Singly Circular Linked List is empty!\n\n");
-        exit(0);
-    }
-
-    printf("\n\t Singly Circular Linked List after deletion of node at given position : ");
+    printf("\n\t -------------------------------------------------------- \n");
+    printf("\n\t Linked List : ");
     displaySCLL(last);
     
-    freeSCLL(last);
+    while (1) {
+        int ch; 
+        printf("\n\t --------------------------------------");
+        printf("\n\t 1. Delete a node at the given position ");
+        printf("\n\t 2. Exit ");
+        printf("\n\t --------------------------------------");
+        printf("\n\t Enter your choice : ");
+        scanf("%d",&ch);
+        printf("\t ---------------------------------------- \n");
+        
+        switch (ch) {
+            case 1:
+                if (last == NULL) {
+                    printf("\n\t Linked List is Empty! \n\n");
+                    exit(0);
+                }
+                last = deletePos(last);
+                break;
+            
+            case 2:
+                freeSCLL(last);
+                last = NULL;
+                printf("\n\t Program Exited Successfully. \n\n");
+                exit(0);
+
+            default: 
+                printf("\n\t Invalid Choice Entered! \n");
+                break;
+        }
+    }
 
     return 0;
     
@@ -55,9 +81,13 @@ SCLLNode* createSCLL() {
 
     do
     {
-        nw = malloc(sizeof(SCLLNode));
+        nw = (SCLLNode*)malloc(sizeof(SCLLNode));
+        if (nw == NULL) {
+            printf("\n\t Memory Allocation Failed! \n\n");
+            exit(1);
+        }
 
-        printf("\n\tEnter Number and Name : ");
+        printf("\n\t Enter Number and Name : ");
         scanf("%d %s", &(nw -> number), nw -> name);
 
         nw -> next = nw;
@@ -72,7 +102,7 @@ SCLLNode* createSCLL() {
         
         last = nw;
 
-        printf("Do you want to enter more records ( yes(1) / no(0) ) : ");
+        printf("\n\t Do you want to enter more records ( yes(1) / no(0) ) : ");
         scanf("%d", &cnt);
     } while (cnt != 0);
 
@@ -82,7 +112,7 @@ SCLLNode* createSCLL() {
 
 SCLLNode* deletePos(SCLLNode *last) {
 
-    int pos;
+    int pos,flag = 0;
     SCLLNode *d = NULL,*prev = NULL;
     d = last -> next;
 
@@ -90,6 +120,7 @@ SCLLNode* deletePos(SCLLNode *last) {
     scanf("%d",&pos);
 
     if (pos == 1) {
+        flag = 1;
         if (d == last) {
             free(d);
             d = NULL;
@@ -103,20 +134,26 @@ SCLLNode* deletePos(SCLLNode *last) {
         for (prev = d,d = d -> next,i = 1 ; i < (pos - 1) && d != last ; prev = prev -> next,d = d -> next,i++);
 
         if (d != last) {
+            flag = 1;
             prev -> next = d -> next;
             free(d);
             d = NULL;
         } else {
             if (i == (pos - 1)) {
+            flag = 1;
             prev -> next = d -> next;
             last = prev;
             free(d);
             d = NULL;
             } else {
-                printf("\n\t Invalid Position entered!\n\n");
-                exit(0);
+                printf("\n\t Invalid Position entered! \n");
             }
         }
+    }
+
+    if (flag == 1) {
+        printf("\n\t Linked List after deletion of node at the given position : ");
+        displaySCLL(last);
     }
 
     return last;
@@ -127,7 +164,7 @@ void displaySCLL(SCLLNode *last) {
     
     SCLLNode *d = NULL;
     d = last -> next;
-    printf("\n\n|_Head_| ");
+    printf("\n\n\t |_Head_| ");
 
     for ( ; d != last ; d = d -> next )
         printf("--> |_%d_|_%s_|_%p_| ", d -> number, d -> name, d -> next);
