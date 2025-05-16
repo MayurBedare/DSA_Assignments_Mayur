a44aa4122ab0e7ba3ef01bced7f93e17bec85632
MayurBedare <140893488+MayurBedare@users.noreply.github.com>
2025-05-17 00:21:37 +0530

    Doubly Linked List Assignment No. 16

--- 03-Doubly Linked List/16-Assignment/A-16.c
+++ 03-Doubly Linked List/16-Assignment/A-16.c
@@ -62,7 +62,10 @@ DLLNode* createDLL() {
         }
 
         printf("\n\tEnter Number and Name : ");
-        scanf("%d %s", &(nw -> number), nw -> name);
+        scanf("%d", &(nw -> number));
+        getchar();
+        fgets(nw -> name,20,stdin);
+        nw -> name[strcspn(nw -> name,"\n")] = '\0';
 
         nw -> next = NULL;
 
