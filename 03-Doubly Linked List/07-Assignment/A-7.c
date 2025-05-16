516994ac6109fbc8a410be37d6bf38afae6be3c6
MayurBedare <140893488+MayurBedare@users.noreply.github.com>
2025-05-17 00:20:04 +0530

    Doubly Linked List Assignment No. 7

--- 03-Doubly Linked List/07-Assignment/A-7.c
+++ 03-Doubly Linked List/07-Assignment/A-7.c
@@ -4,6 +4,7 @@
 
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
 typedef struct DLLNode
 {
@@ -85,7 +86,10 @@ DLLNode* createDLL() {
         }
 
         printf("\n\t Enter Number and Name : ");
-        scanf("%d %s", &(nw -> number), nw -> name);
+        scanf("%d", &(nw -> number));
+        getchar();
+        fgets(nw -> name,20,stdin);
+        nw -> name[strcspn(nw -> name,"\n")] = '\0';
 
         nw -> next = NULL;
 
