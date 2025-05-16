b2db4f64a49f2077fc305216ba4c85df77dd404a
MayurBedare <140893488+MayurBedare@users.noreply.github.com>
2025-05-17 00:20:46 +0530

    Doubly Linked List Assignment No. 11

--- 03-Doubly Linked List/11-Assignment/A-11.c
+++ 03-Doubly Linked List/11-Assignment/A-11.c
@@ -9,6 +9,7 @@
 
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
 typedef struct DLLNode
 {
@@ -102,7 +103,10 @@ DLLNode* createDLL() {
         }
 
         printf("\n\tEnter Number and Name : ");
-        scanf("%d %s", &(nw -> number), nw -> name);
+        scanf("%d", &(nw -> number));
+        getchar();
+        fgets(nw -> name,20,stdin);
+        nw -> name[strcspn(nw -> name,"\n")] = '\0';
 
         nw -> next = NULL;
 
@@ -141,7 +145,10 @@ DLLNode* insert(DLLNode *head) {
     }
     
     printf("\n\tEnter the number and name of new inserted node at Given position : ");
-    scanf("%d %s", &(nw -> number), nw -> name);
+    scanf("%d", &(nw -> number));
+    getchar();
+    fgets(nw -> name,20,stdin);
+    nw -> name[strcspn(nw -> name,"\n")] = '\0';
 
     if (pos == 1) {
         nw -> next = head;
