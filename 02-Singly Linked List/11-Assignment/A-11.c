bb25e63c430123a9bfe7a3e7997a7d2cc6d063e1
MayurBedare <140893488+MayurBedare@users.noreply.github.com>
2025-05-17 00:17:17 +0530

    Singly Linked List Assignment No. 11

--- 02-Singly Linked List/11-Assignment/A-11.c
+++ 02-Singly Linked List/11-Assignment/A-11.c
@@ -4,6 +4,7 @@
 
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
 typedef struct SLLNode
 {
@@ -82,7 +83,10 @@ SLLNode* createSLL() {
         }
 
         printf("\n\t Enter Number and Name : ");
-        scanf("%d %s", &(nw -> number), nw -> name);
+        scanf("%d", &(nw -> number));
+        getchar();
+        fgets(nw -> name,20,stdin);
+        nw -> name[strcspn(nw -> name,"\n")] = '\0';
 
         nw -> next = NULL;
 
