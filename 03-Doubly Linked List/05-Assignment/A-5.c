c4d5cb3e511b7356cae1fe06783613489da764de
MayurBedare <140893488+MayurBedare@users.noreply.github.com>
2025-05-17 00:19:46 +0530

    Doubly Linked List Assignment No. 5

--- 03-Doubly Linked List/05-Assignment/A-5.c
+++ 03-Doubly Linked List/05-Assignment/A-5.c
@@ -8,6 +8,7 @@
 
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
 typedef struct DLLNode
 {
@@ -60,7 +61,10 @@ DLLNode* createDLL() {
         }
 
         printf("\n\tEnter Number and Name : ");
-        scanf("%d %s", &(nw -> number), nw -> name);
+        scanf("%d", &(nw -> number));
+        getchar();
+        fgets(nw -> name,20,stdin);
+        nw -> name[strcspn(nw -> name,"\n")] = '\0';
 
         nw -> next = NULL;
 
@@ -96,7 +100,10 @@ DLLNode* insertLast(DLLNode *head) {
     }
     
     printf("\n\tEnter the number and name of new inserted node at Last position : ");
-    scanf("%d %s", &(nw -> number), nw -> name);
+    scanf("%d", &(nw -> number));
+    getchar();
+    fgets(nw -> name,20,stdin);
+    nw -> name[strcspn(nw -> name,"\n")] = '\0';
 
     for (p = head ; p -> next != NULL ; p = p -> next);
     
