b9b0cdd2d379efd0d56bac1234bf617f3c24680b
MayurBedare <140893488+MayurBedare@users.noreply.github.com>
2025-05-18 15:11:24 +0530

    Queue Assignment No. 4

--- 07-Queue/04-Assignment/A-4.c
+++ 07-Queue/04-Assignment/A-4.c
@@ -15,42 +15,44 @@ typedef struct queue
     struct queue *next;
 } que;
 
-que* enqueue(que**,que*,que**,int*);
-que* dequeue(que*,int*);
+que* enqueue(que**,que*,int*,int *);
+que* dequeue(que*,int*,int*);
 int isEmpty(que*,que*);
 int isFull(que*,que*,int);
-void display(que*,que*,que*);
+void display(que*,que*,que*,int);
 
 void main() {
-    que *front = NULL,*rear = NULL,*temp = NULL;
-    int i = -1;
+    que *front = NULL,*rear = NULL,*head = NULL;
+    int i = -1,j = 0;
 
+    printf("\n\t ***** Circular Queue using array *****\n");
     while (1) {
         int ch;
-        printf("\n\t **************************");
-        printf("\n\t Circular Queue using array");
-        printf("\n\t **************************");
+        printf("\n\t --------------------");
         printf("\n\t 1.Enqueue");
         printf("\n\t 2.Dequeue");
         printf("\n\t 3.Exit");
+        printf("\n\t --------------------");
         printf("\n\t Enter your choice : ");
         scanf("%d",&ch);
+        printf("\t --------------------\n");
 
         switch (ch) {
             case 1:
-                // if (!isFull(front, rear,i)) {
-                //     break;
-                // }
-                rear = enqueue(&front, rear, &temp, &i);
-                display(front, rear, temp);
+                if (!isFull(front, rear,i)) {
+                    break;
+                }
+                rear = enqueue(&front, rear, &i, &j);
+                if (rear -> next == rear)    head = rear;
+                display(front, rear, head,j);
                 break;
         
             case 2:
                 if (!isEmpty(front, rear)) { 
                     break;
                 }
-                front = dequeue(front, &i);
-                display(front, rear, temp);      
+                front = dequeue(front, &i,&j);
+                display(front, rear,head,j);      
                 break;
 
             case 3:
@@ -63,36 +65,44 @@ void main() {
     }
 }
 
-que* enqueue(que **front,que *rear,que **temp,int *i) {
-    if (*i == (MAX - 1))
-        rear -> next = (*temp);
+que* enqueue(que **front,que *rear,int *i,int *j) {
+    que *nw = NULL;
+    nw = malloc(sizeof(que));
 
-        que *nw = NULL;
-        nw = malloc(sizeof(que));
-        printf("Enter the number and name to be enqueued : ");
-        scanf("%d", &(nw -> no));
-        getchar();//Clear the buffer using fgets 
-        fgets(nw -> name, sizeof(nw -> name), stdin);
-        nw -> name[strcspn(nw -> name, "\n")] = '\0';//Remove newline character
-        nw -> next = NULL;
-    
+    if (*j != MAX) {
+        printf("\n\t Enter the number and name to be enqueued : ");
+        scanf("%d %s",&(nw -> no),nw -> name);
+        nw -> next = nw;
+        (*j)++;
+
         if (rear == NULL && (*front) == NULL) {
             (*front) = nw;
         } else {
-            (rear) -> next = nw;
+            nw -> next = rear -> next;
+            rear -> next = nw;
         }
-        (rear) = nw;
+        rear = nw;
         (*i)++;
+    } else {
+        if (*i == MAX-2) {
+            rear = rear -> next;
+        }
+        printf("\n\t Enter the number and name to be enqueued : ");
+        scanf("%d %s",&(rear -> no),rear -> name);
+        (*i)++;
+        (*j)++;
+    }
 
     return rear;    
 }
 
-que* dequeue(que *front, int *i) {
+que* dequeue(que *front, int *i,int *j) {
 
     front -> no = 0;
     strcpy(front -> name,"--");
     front = front -> next;
     (*i)--;
+    (*j)--;
 
     return front;
 }
@@ -101,19 +111,13 @@ int isEmpty(que *front,que *rear) {
     if ((front == NULL) && (rear == NULL)) {
         printf("\n\t Queue is Empty!");
         return 0;
-    } else if ((rear -> next == front) && (rear -> no == 0)) {
-        printf("\n\t Queue is Empty!");
-        return 0;
     } else {
         return 1;
     }
 }
 
 int isFull(que *front,que *rear, int i) {
-    if (i == MAX) {
-        printf("\n\t Queue is Full!");
-        return 0;
-    } else if ((rear -> next == front) && (rear -> no != 0)) {
+    if (i == MAX-1) {
         printf("\n\t Queue is Full!");
         return 0;
     } else {
@@ -121,25 +125,53 @@ int isFull(que *front,que *rear, int i) {
     }
 } 
 
-void display(que *front,que *rear,que *temp) {
-    que *p;
+void display(que *front,que *rear,que *head,int j) {
+    que *p = NULL;
     int i = 0;
     if (front == rear) {
-        for (p = temp ; i != MAX ; i++, p = p -> next) {
-            if (front == p)
-                printf("\n\t |_%d_|_%s_| <- Front <- Rear", p -> no, p -> name);
-            else 
-                printf("\n\t |_%d_|_%s_|", p -> no = 0, strcpy(p -> name,"--"));
+        for (p = head ; i != MAX ; i++,head = head -> next) {
+            if (j == (MAX)) {
+                if (front == p) 
+                    printf("\n\t |_%d_|_%s_| <- Front <- Rear", p -> no, p -> name);
+                else 
+                    printf("\n\t |_%d_|_%s_| <- Front <- Rear", p -> no = 0, strcpy(p -> name,"--"));
+            } else { 
+                if (front == p && p != p -> next) {
+                    printf("\n\t |_%d_|_%s_| <- Front <- Rear", p -> no, p -> name);
+                } else if (i == 0) {
+                    printf("\n\t |_%d_|_%s_| <- Front <- Rear", p -> no, p -> name);
+                } else {
+                    printf("\n\t |_%d_|_%s_|", 0, "--");
+                }
+            }
         }
     } else {
-        for (p = temp ; i != MAX ; i++, p = p -> next) {
-            if (front == p) {
-                printf("\n\t |_%d_|_%s_| -> Front", p -> no, p -> name);
-            } else if (rear == p) {
-                printf("\n\t |_%d_|_%s_| -> Rear", p -> no, p -> name);
+        int m = 1,n = 1;
+        for (p = head ; i != MAX ; i++, p = p -> next) {
+            if (j == (MAX)) {
+                if (front == p) {
+                    printf("\n\t |_%d_|_%s_| -> Front", p -> no, p -> name);
+                } else if (rear == p) {
+                    printf("\n\t |_%d_|_%s_| -> Rear", p -> no, p -> name);
+                } else {
+                    printf("\n\t |_%d_|_%s_|", p -> no, p -> name);
+                }
             } else {
-                printf("\n\t |_%d_|_%s_|", p -> no, p -> name);
-            }
+                if (front == p && m != 0) {
+                    m = 0;
+                    printf("\n\t |_%d_|_%s_| -> Front", p -> no, p -> name);
+                } else if (rear == p && n != 0) {
+                    n = 0;
+                    printf("\n\t |_%d_|_%s_| -> Rear", p -> no, p -> name);
+                } else if (front -> next == rear) {
+                        printf("\n\t |_%d_|_%s_|", 0,"--");
+                } else if (front -> next -> next == rear) {
+                    if (rear -> next -> next == front)
+                        printf("\n\t |_%d_|_%s_|", p -> no, p -> name);
+                    else
+                        printf("\n\t |_%d_|_%s_|", 0,"--");
+                }
+            } 
         }
     }
     printf("\n");
