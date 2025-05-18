0358a9e96ccea223982214699dc50df38a6059ef
MayurBedare <140893488+MayurBedare@users.noreply.github.com>
2025-05-18 14:36:39 +0530

    Queue Assignment No. 1

---
+++ 07-Queue/01-Assignment/efficient_version/A-1.c
@@ -0,0 +1,74 @@
+/*
+    A-1 ->  write a c program to Create a queue and perform enqueue and dequeue operations(using array)
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 3
+
+typedef struct Queue {
+    int no;
+    char name[10];
+} queue;
+
+int enqueue(int ,queue*);
+int dequeue(int,int,queue*);
+void display(int,int,queue*);
+
+int main() {
+    queue a[MAX];
+    int front = 0,rear = -1;
+    int ch;
+
+    while (1) {
+        printf("\n\t Queue with array ");
+        printf("\n\t 1. Enqeue");
+        printf("\n\t 2. Dequeue");
+        printf("\n\t 3. Display");
+        printf("\n\t 4. Exit");
+        printf("Enter your choice :");
+        scanf("%d",&ch);
+        switch(ch) {
+            case 1: rear = enqueue(rear,a);break;
+            case 2: front = dequeue(front,rear,a);
+                    if(front == 0)  rear = -1;
+                    break;
+            case 3: display(front,rear,0);break;
+            case 4: exit(0);
+
+            default: printf("Invalid choice");
+        }
+    }
+    return 0;
+}
+
+int enqueue(int rear,queue *a) {
+    if (rear == MAX-1)  printf("Queue is Full!");
+    else {
+        rear++;
+        printf("Eneter no. & name: ");
+        scanf("%d %s",&a[rear].no,&a[rear].name);
+    }
+    return rear;
+}
+
+int dequeue(int front,int rear, queue *a) {
+    if (rear == -1) printf("Queue is Empty!");
+    else if (front == rear) front = 0;
+    else {
+        printf("%d is deleted.",a[front].no);
+        front++;
+    }
+    return front;
+}
+
+void display(int front,int rear,queue *a) {
+    if (rear == -1) {
+        printf("Queue is empty");
+        return;
+    }
+    for (int i = front ; i <= rear ; i++) {
+        printf("%d\t%s\t\t",a[i].no,a[i].name);
+    }
+}
