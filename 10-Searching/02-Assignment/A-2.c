85bdf51f0f59487cc2a9002ac8c2a39988315b63
MayurBedare <140893488+MayurBedare@users.noreply.github.com>
2025-05-18 15:38:35 +0530

    Searching Assignment No. 2

--- 10-Searching/02-Assignment/A-2.c
+++ 10-Searching/02-Assignment/A-2.c
@@ -16,7 +16,7 @@ int main() {
     printf("\n\t The Time Complexity of the Binary Search [worst cases]: O(log n) \n");
     printf("\t ------------------------------------------------------------------ \n");
 
-    int *arr = NULL,i = -1,key;
+    int *arr = NULL,flag = 0,key;
     arr = (int*)malloc(SIZE*sizeof(int));    
     if (arr == NULL) {
         printf("\n\t Memory Allocation Failed! \n\n");
@@ -33,22 +33,22 @@ int main() {
     printf("\n\t Enter the array element to be searched : ");
     scanf("%d",&key);
 
-    i = binarySearch(arr,i,SIZE,key);
+    flag = binarySearch(arr,flag,SIZE,key);
 
-    if (i != -1)    printf("\n\t The array element %d is found at arr[%d] \n\n",key,i);     
+    if (flag == 1)    printf("\n\t The array %d element is found \n\n",key);     
     else    printf("\n\t The array element %d is not found! \n\n",key);  
 
     return 0;   
 }
 
-int binarySearch(int arr[],int index,int n, int key) {
+int binarySearch(int arr[],int flag,int n, int key) {
     int low = 0,high = n-1,mid;
 
     while (low <= high) {
         mid = (high + low)/2;
 
         if (key == arr[mid]) {
-            index = mid;
+            flag = 1;
             break;
         } 
 
@@ -59,5 +59,5 @@ int binarySearch(int arr[],int index,int n, int key) {
         } 
     }
 
-    return index;
+    return flag;
 }
