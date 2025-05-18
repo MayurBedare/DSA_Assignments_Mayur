75c17dd33954ed4d5de244e6899f0b0b78bc4cfc
MayurBedare <140893488+MayurBedare@users.noreply.github.com>
2025-05-18 15:46:19 +0530

    Pointers Assignment No. 7

--- 01-C Revision/01-Pointers/07-Assignment/A-7.c
+++ 01-C Revision/01-Pointers/07-Assignment/A-7.c
@@ -18,21 +18,21 @@ typedef struct Student
 
 int main()
 {
-    Student *p;
+    Student p;
     
     printf("\n\tEnter Student Name : ");
-    scanf("%s", p -> name);
+    scanf("%s", p.name);
     printf("\n\tEnter Student Roll No. : ");
-    scanf("%d", &(p -> roll_no));
+    scanf("%d", &(p.roll_no));
     printf("\n\tEnter Student Division : ");
-    scanf("%s", p -> div);
+    scanf("%s", p.div);
     printf("\n\tEnter Student Standard : ");
-    scanf("%s", p -> std);
+    scanf("%s", p.std);
 
-    printf("\n\tStudent Name \t\t: %s", p -> name);
-    printf("\n\tStudent Roll No. \t: %d", p -> roll_no);
-    printf("\n\tStudent Division \t: %s", p -> div);
-    printf("\n\tStudent Standard \t: %s\n\n", p -> std);
+    printf("\n\tStudent Name \t\t: %s", p.name);
+    printf("\n\tStudent Roll No. \t: %d", p.roll_no);
+    printf("\n\tStudent Division \t: %s", p.div);
+    printf("\n\tStudent Standard \t: %s\n\n", p.std);
 
     return 0;
 }
