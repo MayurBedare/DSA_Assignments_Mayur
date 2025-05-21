572e5bf588a2c30a52d4ae095aa517887c2f9570
MayurBedare <140893488+MayurBedare@users.noreply.github.com>
2025-05-21 21:50:26 +0530

    String Revision Assignment No. 2

--- 08-String Revision/02-Assignment/A-2.c
+++ 08-String Revision/02-Assignment/A-2.c
@@ -2,21 +2,23 @@
     A-2 ->  Accept two strings and concatinate the contents into 3rd string
 */
 
-/* 
-    Input ->    string1[] = "Hello"
-                string2[] = "World!"        
-*/
-
 #include<stdio.h>
+#include<string.h>
 
 int main() {
     char string1[50];
     char string2[50];
+    char newstring[50];
 
     printf("\n\t Enter the first string  : ");
-    scanf("%s",string1);
+    fgets(string1,50,stdin);
+    string1[strcspn(string1,"\n")] = '\0';
     printf("\n\t Enter the second string : ");
-    scanf("%s",string2);
+    fgets(string2,50,stdin);
+    string2[strcspn(string2,"\n")] = '\0';
+
+    strcpy(newstring,string1);
+    strcat(newstring,string2);
 
     int i;
     for (i = 0 ; string1[i] != '\0' ; i++);
@@ -25,16 +27,8 @@ int main() {
     }
     string1[i] = '\0';
 
+    printf("\n\t The final concatenated string (using inbuilt function) : %s \n",newstring);
     printf("\n\t The final concatenated string : %s \n\n",string1);
 
     return 0;   
 }
-
-/*
-    Output ->    
-                Enter the first string  : Hello
-
-                Enter the second string : World!
-
-                The final concatenated string : HelloWorld!                            
-*/
