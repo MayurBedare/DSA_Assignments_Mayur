05d8e3e94f869b40ee3dfa213434cf6fe782759f
MayurBedare <140893488+MayurBedare@users.noreply.github.com>
2025-05-23 00:04:05 +0530

    1D Array Assignment No. 10

--- 01-C Revision/05-1D Array/10-Assignment/A-10.c
+++ 01-C Revision/05-1D Array/10-Assignment/A-10.c
@@ -9,7 +9,7 @@
 #define SIZE 10
 
 int main() {
-    int positive = 0,sum = 0,largest,largest_negative = 0;
+    int max_sum = 0,index = 0,size_subarr = 1,x = 1,check_all_minus = 0;
     int *arr = NULL;
     arr = (int*)malloc(SIZE*sizeof(int));
     if (arr == NULL) {
@@ -23,24 +23,46 @@ int main() {
     for (int i = 0 ; i < SIZE ; i++) {
         printf("\n\t Enter arr[%d] : ",i);
         scanf("%d",&arr[i]);
-         
-        if (i == 0) largest = arr[i];
-        else if (arr[i] > largest)   largest = arr[i];
 
-        if (arr[i] > 0) {
-            positive = 1;
-            sum = sum + arr[i];
-        } else {
-            if(largest_negative == 0)   largest_negative = arr[i];
-            else if (arr[i] > largest_negative) largest_negative = arr[i];
+        if (arr[i] < 0) {
+            check_all_minus++;
         }
     }
 
-    if (positive) {
-        printf("\n\t The subarray with maximum sum of given arr[%d] : %d \n\n",SIZE,sum);
-    } else {
-        printf("\n\t The subarray with maximum sum of given arr[%d] : %d \n\n",SIZE,largest_negative);
+    printf("\n\t The given arr[%d] : \n",SIZE);
+    for (int i = 0 ; i < SIZE ; i++) {
+        if (i == 0) printf("\n\t |_%d_|",arr[i]);
+        else    printf("_%d_|",arr[i]);
     }
+
+    if (check_all_minus == SIZE) {
+        max_sum = arr[0];
+    }
+
+    while (x <= SIZE) {
+        for (int m = 0 ; m < SIZE ; m++) {
+            int sum = 0;
+            for (int n = m ; n < m+x && n < SIZE ; n++) {
+                sum = sum + arr[n];
+            }
+            if (sum > max_sum) {
+                max_sum = sum;
+                index = m,size_subarr = x;
+            }
+        }
+        if (x == 1 && check_all_minus == SIZE) {
+            break;
+        }
+        x++;
+    }
+
+    printf("\n\n\t The subarray with maximum sum : \n");
+    for (int j = index ; j < index + size_subarr ; j++) {
+        if (j == index) printf("\n\t |_%d_|",arr[j]);
+        else    printf("_%d_|",arr[j]);
+    }
+
+    printf("\n\n\t The maximum sum of subarray of arr[%d] : %d \n\n",SIZE,max_sum);
 
     free(arr);
     arr = NULL;
