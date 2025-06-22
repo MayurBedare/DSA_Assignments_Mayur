ca497c3e5b66e13520580fda6d31e6130ae8cd2d
MayurBedare <140893488+MayurBedare@users.noreply.github.com>
2025-06-22 22:34:55 +0530

    Tree Assignment No. 11

---
+++ 13-Tree/01-BST/11-Assignment/m1/main.c
@@ -0,0 +1,22 @@
+/*
+    A-11 ->  Wrtie a C program to find the maximum element in the BST.
+*/
+
+#include<stdio.h>
+#include "a_11.h"
+
+int main() {
+    printf("\n\t ***** Creating BST ***** \n");
+
+    BST *root = NULL;
+
+    root = create(root);
+
+    printf("\n\t Printing of the given Tree in Inorder Traversal : \n\n\t");
+    inorder(root);
+    printf("\n");
+
+    printf("\n\t Maximum element: %d \n", findMax (root));
+
+    return 0;   
+}
