7f7e6e5cd3b255ce40bcc8f34f71a962ae42ac0e
MayurBedare <140893488+MayurBedare@users.noreply.github.com>
2025-06-22 22:33:21 +0530

    Tree Assignment No. 7

---
+++ 13-Tree/01-BST/07-Assignment/m1/a_7.h
@@ -0,0 +1,14 @@
+#ifndef BST_H //if  not defined yet
+#define BST_H //define if now(first time use)
+
+typedef struct BST{
+    int data;
+    struct BST* left;
+    struct BST* right;
+} BST;
+
+BST* create(BST *root);
+int searchRecursive (const BST* root,int key);
+void inorder (BST *root);
+
+#endif
---
+++ 13-Tree/01-BST/07-Assignment/m1/main.c
@@ -0,0 +1,29 @@
+/*
+    A-7 ->  Wrtie a C program to create BST and Search an element in BST using Recursion
+*/
+
+#include<stdio.h>
+#include "a_7.h"
+
+int main() {
+    printf("\n\t ***** Creating BST ***** \n");
+
+    BST *root = NULL;
+    int val;
+
+    root = create(root);
+
+    printf("\n\t Printing of the given Tree in Inorder Traversal : \n\n\t");
+    inorder(root);
+    printf("\n");
+
+    printf("\n\t Enter key to Search:");
+    scanf("%d", & val);
+    if(searchRecursive (root,val))
+    printf("\n\t Found \n");
+    else
+    printf("\n\t Not found \n");
+    printf("\n");
+
+    return 0;   
+}
