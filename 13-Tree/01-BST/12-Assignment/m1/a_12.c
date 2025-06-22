71fa3298978fa1eb3ea1705abafc21d72869c33f
MayurBedare <140893488+MayurBedare@users.noreply.github.com>
2025-06-22 22:35:36 +0530

    Tree Assignment No. 12

---
+++ 13-Tree/01-BST/12-Assignment/m1/a_12.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "a_12.h"
+
+BST* create(BST* root) {
+    BST *nw,*p = NULL;
+    int ch;
+    do {
+        nw = (BST*)malloc(sizeof(BST));
+        if (nw == NULL) {
+            printf("\n\t Memory Allocation Failed \n");
+            exit(1);
+        }
+
+        printf("\n\t Enter the Data ie Number : ");
+        scanf("%d",&(nw -> data));
+        nw -> left = nw -> right = NULL;
+
+        if (root == NULL) {
+            root = nw;
+        } else {
+            p = root;
+            while(1) {
+                if (nw -> data < p -> data) {
+                    if (p -> left == NULL) {
+                        p -> left = nw;
+                        break;
+                    } 
+                    p = p -> left;
+                } else if (nw -> data > p -> data) {
+                    if (p -> right == NULL) {
+                        p -> right = nw;
+                        break;
+                    } 
+                    p = p -> right;
+                } else {
+                    printf("\n\t No is already exist. \n");
+                    break;
+                }
+            }
+        }
+        printf("\n\t Do you want to enter more numbers ( Yes (1) / No (0) ) : ");
+        scanf("%d",&ch);
+    } while (ch != 0);
+
+    return root;
+}
+
+int findHeight(const BST *root) {
+    if (!root) return 0;
+
+    int LH = findHeight(root->left);
+    int RH = findHeight(root->right);
+    return(LH>RH ? LH : RH) + 1;
+}
+
+void inorder(BST *root) {
+    if (root) {
+        inorder(root->left);
+        printf(" %d ",root->data);
+        inorder(root->right);
+    } 
+}
---
+++ 13-Tree/01-BST/12-Assignment/m1/main.c
@@ -0,0 +1,23 @@
+/*
+    A-12 ->  Wrtie a C program to calculate the height (depth) of the BST.
+             The depth of a node is the number of edges present in path from the root node of a tree to that node.
+*/
+
+#include<stdio.h>
+#include "a_12.h"
+
+int main() {
+    printf("\n\t ***** Creating BST ***** \n");
+
+    BST *root = NULL;
+
+    root = create(root);
+
+    printf("\n\t Printing of the given Tree in Inorder Traversal : \n\n\t");
+    inorder(root);
+    printf("\n");
+
+    printf("\n\t Height of BST: %d \n", findHeight (root));
+
+    return 0;   
+}
