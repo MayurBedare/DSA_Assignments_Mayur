8c49514cf66aafefe229fb166ab0b4d924851174
MayurBedare <140893488+MayurBedare@users.noreply.github.com>
2025-07-29 19:56:13 +0530

    Recursion Assignment No. 1

--- 12-Recursion/01-Assignment/A-1.c
+++ 12-Recursion/01-Assignment/A-1.c
@@ -1,29 +1,41 @@
 /*
-    A-1 ->  Accept  N  numbers and calculate sum of all numbers using recursion
+    A-1 ->  Accept N numbers and calculate sum of all numbers using recursion
 */
 
 #include<stdio.h>
+#include<stdlib.h>
 
-int sum(int);
+int sum(int[],int);
 
 int main() {
     printf("\n\t ***** Sum of N numbers using Recursion ***** \n");
     
     int n,s;
 
-    printf("\n\t Enter the value of N to find sum of N natural no.s : ");
+    printf("\n\t Total numbers to be entered : ");
     scanf("%d",&n);
 
-    s = sum(n);
+    int *arr = (int*)malloc(sizeof(int)*n);
+    if (!arr) {
+        printf("\n\t Memory Allocation Failed! ");
+        exit(1);
+    }
+
+    printf("\n\t Enter the numbers one by one -> \n\t");
+    for (int i = 0 ; i < n ; i++) {
+        scanf("%d",&arr[i]);
+    }
+
+    s = sum(arr,n-1);
 
     printf("\n\t The sum of %d natural no.s : %d \n\n",n,s);
 
     return 0;   
 }
 
-int sum (int n) {
-    if (n == 0) return 0;
-    else    return n + sum(n - 1);
+int sum (int arr[],int n) {
+    if (n == -1) return 0;
+    else    return arr[n] + sum(arr,n-1);
 }
 
 
